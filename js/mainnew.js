b357401705c391d3d3bab9bc0dec7581e8bc967d
kaosbeat <kasper.jordaens@gmail.com>
2015-01-28 11:47:50 +0100

    some cleanup,simplified video code works!

--- js/mainnew.js
+++ js/mainnew.js
@@ -1,89 +1,11 @@
 var pageSize = 150;
 var s;
-
 var page = 0;
 
-// function post(url, player, action, value) {
-//     var data = {
-//       method: action
-//     };
-
-//     if (value) {
-//         data.value = value;
-//     }
-
-//     var message = JSON.stringify(data);
-//     player[0].contentWindow.postMessage(data, url);
-// }
-
-
-// function initSkrollr(){
-//     // init Vimeo players
-//     var printer = $('#printer');
-//     var printerurl = window.location.protocol + printer.attr('src').split('?')[0];
-//     var streamstore = $('#streamstore');
-//     var streamstoreurl = window.location.protocol + streamstore.attr('src').split('?')[0];
-
-//     var mediahack = $('#mediahack');
-//     var mediahackurl = window.location.protocol + mediahack.attr('src').split('?')[0];
-
-//     var mediamusic = $('#mediamusic');
-//     var mediamusicurl = window.location.protocol + mediamusic.attr('src').split('?')[0];
-
-//     s = skrollr.init({
-//     	render:  function(data){
-//             // $('.debug').html(data.curTop);
-//     		console.log(data.curTop);
-//     		if (data.curTop > 249 && data.curTop < 401) {
-//                 post(printerurl, printer, 'setVolume', '0');
-//     			post(printerurl, printer, 'play');
-//                 // console.log("lets play");
-//     		}
-//             if (data.curTop > 400 || data.curTop < 250) {
-//                 post(printerurl, printer, 'pause');
-//                 // console.log("lets pause");
-//             }
-//             if (data.curTop == 1000) {
-//                 post(streamstoreurl, streamstore, 'setVolume', '0');
-//                 post(streamstoreurl, streamstore, 'seekTo', '44');
-//                 post(streamstoreurl, streamstore, 'play');
-//             }
-//             if (data.curTop > 1000 && data.curTop < 1201) {
-//                 post(streamstoreurl, streamstore, 'setVolume', '0');
-//                 post(streamstoreurl, streamstore, 'play');
-//                 // console.log("lets play");
-//             }
-//             if (data.curTop > 1200 || data.curTop < 1000) {
-//                 post(streamstoreurl, streamstore, 'pause');
-//                 // console.log("lets pause");
-//             }
-//             if (data.curTop > 1600 && data.curTop < 1800) {
-//                 post(mediahackurl, mediahack, 'setVolume', '0');
-//                 post(mediahackurl, mediahack, 'play');
-//                 // console.log("lets play");
-//             }
-//             if (data.curTop > 1799 || data.curTop < 1601) {
-//                 post(mediahackurl, mediahack, 'pause');
-//                 // console.log("lets pause");
-//             }
-
-//             if (data.curTop > 1799 && data.curTop < 2001) {
-//                 post(mediamusicurl, mediamusic, 'setVolume', '0');
-//                 post(mediamusicurl, mediamusic, 'play');
-//                 // console.log("lets play");
-//             }
-//             if (data.curTop > 2000 || data.curTop < 1800) {
-//                 post(mediamusicurl, mediamusic, 'pause');
-//                 // console.log("lets pause");
-//             }
-
-
-
-//     	}
-//     });
-
-// }
-
+var printer, streamstore, mediahack, mediamusic;
+function initVideos() {
+    printer = $f( $('#printer')[0]);
+}
 
 function initKeys(){
 
@@ -124,12 +46,17 @@ function pageDown(){
         if (page == 18) {
             return;
         } else {
-        page++;
-        $( "#main section:eq(" + (page - 1) + ")" ).addClass('fade');
-        $( "#main section:eq(" + page +")" ).addClass('slideUp');
-        console.log("down, page is " + page + "title = " + $( "#main section:eq(" + (page - 1) +")" ).attr("id") );
-        // $('.debug').html("down");
+
+            page++;
+            $( "#main section:eq(" + (page - 1) + ")" ).addClass('fade');
+            $( "#main section:eq(" + page +")" ).addClass('slideUp');
+            console.log("down, page is " + page + "title = " + $( "#main section:eq(" + (page - 1) +")" ).attr("id") );
+            // $('.debug').html("down");
+            if (page == 2) {
+                printer.api('play');
+            }
         }
+
     }
 function pageUp(){
         // s.animateTo((getcurrentPage()-1)*pageSize);
@@ -152,6 +79,10 @@ var hammerOptions = {}
 $(function() {
     // init();
     // initSkrollr();
+
+
+
+    initVideos();
     initKeys();
     initSpace();
 
